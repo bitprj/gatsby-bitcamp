146eff8074b6481afed59431b37ddea016e29083
jeffchhen <56717962+jeffchhen@users.noreply.github.com>
2020-04-02 23:41:08 -0700

    update7
    casing

---
+++ src/components/Header2.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Link, graphql, useStaticQuery } from 'gatsby'
+import headerStyles from './header.module.scss'
+const Header2 = () => {
+    const data = useStaticQuery(graphql`
+      query {
+          site{
+            siteMetadata {
+                title
+                author
+            }
+          }
+      }
+    `)
+    return(
+        <header className={headerStyles.header}>
+            <h1 >
+                <Link className={headerStyles.title} to="/">
+                    {data.site.siteMetadata.title}
+                </Link>
+            </h1>
+            <nav>
+            <ul className={headerStyles.navlist}>
+                <li>
+                    <Link className={headerStyles.navitem} activeClassName={headerStyles.activeNavItem} to='/ContactMe'>
+                        Baasdas 
+                    </Link>
+                </li>
+                <li>
+                    <Link className={headerStyles.navitem} activeClassName={headerStyles.activeNavItem} to='/About'>
+                         About 
+                    </Link>
+                </li>
+                <li>
+                    <Link className={headerStyles.navitem} activeClassName={headerStyles.activeNavItem}to='/Blog'>
+                         Blog 
+                    </Link>
+                </li>
+                <li>
+                    <Link className={headerStyles.navitem} activeClassName={headerStyles.activeNavItem} to='/..'> 
+                    Index
+                    </Link>
+                </li>
+            </ul>
+            </nav>
+        </header>
+        )
+}
+
+export default Header2
